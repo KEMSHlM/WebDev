f51fe3dad2758be1e4adda8952575ac95a7c9804
KEMSHlM <koyama.kenshin.748@s.kyushu-u.ac.jp>
2024-01-28 14:24:07 +0900

    temp

---
+++ Threejs/010_Box/main.js
@@ -0,0 +1,35 @@
+ import * as THREE from "three";
+
+// サイズを指定
+const width = 960;
+const height = 540;
+
+// レンダラーを作成
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#myCanvas"),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(width, height);
+
+// シーンを作成
+const scene = new THREE.Scene();
+
+// カメラを作成
+const camera = new THREE.PerspectiveCamera(45, width / height);
+camera.position.set(0, 0, +1000);
+
+// 箱を作成
+const geometry = new THREE.BoxGeometry(400, 400, 400);
+const material = new THREE.MeshNormalMaterial();
+const box = new THREE.Mesh(geometry, material);
+scene.add(box);
+
+tick();
+
+// 毎フレーム時に実行されるループイベントです
+function tick() {
+  box.rotation.y += 0.01;
+  renderer.render(scene, camera); // レンダリング
+
+  requestAnimationFrame(tick);
+}
